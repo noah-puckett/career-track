bd055c1cc1c39e7f8559ed754a7a3129e95543c3
Noah Puckett <noah.ingram.puckett@gmail.com>
2019-06-18 14:46:52 -0700

    wrapped server start function in exportable function

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const http = require("http");
-
+    
 function onRequest(request, response) {
+    console.log("Request Received!");
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.write("Hello World");
     response.end();
 };
 
 http.createServer(onRequest).listen(8888);
+
+console.log("Server has started!");
