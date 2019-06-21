231a13b8623702cae172be599f3c5dfa1a2c5597
Noah Puckett <noah.ingram.puckett@gmail.com>
2019-06-20 20:38:12 -0700

    changed route function to do things I do not understand

--- router.js
+++ router.js
@@ -1,5 +1,12 @@
-function route(pathName) {
+function route(handle, pathName) {
     console.log('About to rout a request for ' + pathName);
+
+    if(typeof handle[pathName] === 'function') {
+        handle[pathName]();
+    }
+    else {
+        console.log("No request handler found for " + pathName);
+    }
 }
 
 exports.route = route;
