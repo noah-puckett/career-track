9e62a79c4c4848823ebeced7f88d618e7c7550e6
Noah Puckett <noah.ingram.puckett@gmail.com>
2019-06-20 20:26:14 -0700

    added requestHandlers module, created start and upload functions

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const url = require("url");
 function start(route) {
     function onRequest(request, response) {
         const pathName = url.parse(request.url).pathName;
-        console.log("Request for" + pathName + "Received!");
+        console.log("Request for " + pathName + " Received!");
 
         route(pathName);
 
